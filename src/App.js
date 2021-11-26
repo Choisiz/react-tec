0347344bd507ed0ccf49e0caab70b9318fee7df5
cky <dnjsvltm327@gmail.com>
2021-11-26 13:25:11 +0900

    9-compoent styling

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React, { useState } from "react";
 import Average from "./HooksTutorial/Average";
 import Counter from "./HooksTutorial/Counter";
 import SassComponent from "./SassComponent";
+import StyledComponent from "./StyledComponent";
 
 const App = () => {
-  return <SassComponent />;
+  return <StyledComponent />;
 };
 
 export default App;

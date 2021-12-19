109ca0d326a0004034073d6ba778e6dcde27a194
dnjsvltm201311681 <dnjsvltm201311681@gmail.com>
2021-12-20 01:30:22 +0900

    tec1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
+/* eslint-disable */
 import React, { useState } from "react";
-import Average from "./HooksTutorial/Average";
-import Counter from "./HooksTutorial/Counter";
-
-const App = () => {
-  return <Average />;
-};
+import "./App.css";
+function App() {
+  const [title, setTitle] = useState(["코트추천", "옷추천", "바지추천"]);
+  const [good, setGood] = useState(0);
+  let today = "크리스마스";
+  return (
+    <div className="App">
+      <div className="black-nav" black-nav>
+        <div>개발 블로그</div>
+      </div>
+      <div className="list">
+        <h1
+          onClick={() => {
+            let array = [...title];
+            array[0] = "여자코트";
+            setTitle(array);
+          }}
+        >
+          {title[0]}
+          <span
+            onClick={() => {
+              setGood(good + 1);
+            }}
+          >
+            👍{good}
+          </span>
+        </h1>
+        <p>12월 25일</p>
+        <hr />
+        <h1>{title[1]}</h1>
+        <p>12월 25일</p>
+        <hr />
+        <h1>{title[2]}</h1>
+        <p>12월 25일</p>
+        <hr />
+      </div>
+    </div>
+  );
+}
 
 export default App;
--- src/HooksTutorial/Average.js
+++ src/HooksTutorial/Average.js
@@ -1,26 +1,28 @@
 import { useEffect, useState, useMemo } from "react";
 
-const getAverage = (numbers) => {
-  console.log("계산중");
-  if (numbers.length === 0) return 0;
-  const sum = numbers.reduce((a, b) => (a = b));
-  return sum / numbers.length;
-};
-
 const Average = () => {
   const [list, setList] = useState([]);
   const [number, setNumber] = useState("");
+  const [re, setRe] = useState(0);
 
   const onChange = (e) => {
     setNumber(e.target.value);
   };
   const onInsert = (e) => {
     const nextLine = list.concat(parseInt(number));
+    setRe(0);
     setList(nextLine);
     setNumber("");
+    getAverage(list);
+  };
+  const getAverage = (numbers) => {
+    console.log("계산중");
+    if (numbers.length === 0) return 0;
+    const sum = numbers.reduce((a, b) => a + b);
+    const result = sum / numbers.length;
+    setRe(result);
   };
 
-  const avg = useMemo(() => getAverage(list), [list]);
   return (
     <div>
       <input value={number} onChange={onChange} />
@@ -32,7 +34,7 @@ const Average = () => {
       </ul>
       <div>
         <b>평균값</b>
-        {avg}
+        {parseInt(re)}
       </div>
     </div>
   );
